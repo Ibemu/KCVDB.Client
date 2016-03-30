703977dda909cb4ee30a42dd7be61f6d242b7c7e
biikame <biikame@users.noreply.github.com>
2016-03-30 17:56:46 +0900

    Clients.Senders以下のファイルの一部をルートに移動

---
+++ Source/KCVDB.Client/Clients/IApiDataSender.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace KCVDB.Client.Clients.Senders
+{
+	interface IApiDataSender : IDisposable
+	{
+		Task<ISentApiData> SendData(ApiData data);
+	}
+}
---
+++ Source/KCVDB.Client/SentApiDataRequestBodyFlags.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KCVDB.Client
+{
+	[Flags]
+	public enum SentApiDataRequestBodyFlags
+	{
+		ByteArray = 1,
+		String = 2
+	}
+}
